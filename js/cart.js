b75ba50ca74c13ba929390d873d6ddf71dabeac0
Akshata Desai <AkshataDesai@quickenloans.com>
2018-02-10 18:46:47 -0500

    Pop up for each item,except for burger and Functionality related to pop up

--- js/cart.js
+++ js/cart.js
@@ -4,85 +4,97 @@ var menuCard = {
         'productName': 'Peanut Crusted Cajun BlackBean Burger',
         'productType': 'Burger',
         'price': '2.50',
-        'description': 'Hand Made patty seared on the grill with black beans, spanish peanuts, rice flour for crunch & our house cajun spice blend.',
-        'code':'B1'
+        'description': 'Hand Made patty seared on the grill with black beans, spanish peanuts, rice flour for crunch & our house cajun spice blend',
+        'code':'B1',
+        'imageUrl':"images/burger.jpg"
     },
     'B2': {
         'productName': 'Mean Green Protein Pea Burger',
         'productType': 'Burger',
         'price': '2.50',
         'description': 'Hand made patty seared on the grill and packed with protein from loads of green peas dusted with a savory spice blend',
-        'code': 'B2'
+        'code': 'B2',
+        'imageUrl':"images/burger.jpg"
     },
     'B3': {
         'productName': 'Golden Crunchy Potato Burger',
         'productType': 'Burger',
         'price': '2.00',
         'description': 'A classic from the streets of India.. potatoes, a protein flour blend and secret spices make up this craveable, crunchy street food delight.',
-        'code': 'B3'
+        'code': 'B3',
+        'imageUrl':"images/burger.jpg"
     },
     'B4': {
         'productName': 'The Love Seed Beet them All Burger',
         'productType': 'Burger',
         'price': '2.00',
         'description': 'Fresh shredded beets, roasted sunflower seeds, flax seed, buck wheat, chickpea flour and just the right spice',
-        'code': 'B4'
+        'code': 'B4',
+        'imageUrl':"images/burger.jpg"
     },
     'B5': {
         'productName': 'Golden Crunchy Bird\'s Nest Burger',
         'productType': 'Burger',
         'price': '2.50',
         'description': 'A sell out every rock show we ever set up at.. think coleslaw deepfried in a healthy chickpea flour base with cajun spice.',
-        'code': 'B5'
+        'code': 'B5',
+        'imageUrl':"images/burger.jpg"
     },
     'B6': {
         'productName': 'Tamarind BBQ Jackfruit Sandwich',
         'productType': 'Burger',
         'price': '2.50',
         'description': 'Never had jackfruit',
-        'code': 'B6'
+        'code': 'B6',
+        'imageUrl':"images/burger.jpg"
     },
     'W1': {
         'productName': 'Butter Paneer',
         'productType': 'Wraps',
         'price': '5.50',
         'description': 'Wrap',
-        'code': 'W1'
+        'code': 'W1',
+        'imageUrl':"images/wraps.jpg"
     },
     'W2' : {
     	'productName': 'Spicy Manchurian',
         'productType' : 'Wraps',
         'price': '5.50',
-        'description': 'Wrap',
-        'code': 'W2'
+        'description': 'Spiced crushed chickpeas rolled in colocasia leaves surrounded with mixture of baked multigrain papdi, topped with mixture of potatoes, peas and chickpea, hut-k special sauces and garnished with chickpea flour savories. Allergy information: contains wheat, chickpeas. Vegan. Can be made gluten free. ',
+        'code': 'W2',
+        'imageUrl':"images/wraps.jpg"
     },
     'W3' : {
     	'productName': 'Grilled Fajitas',
         'productType' : 'Wraps',
         'price': '5.50',
         'description': 'Wrap',
-        'code': 'W3'
+        'code': 'W3',
+        'imageUrl':"images/wraps.jpg"
     },
     'W4' : {
     	'productName': 'Falafel',
     	'productType' : 'Wraps',
         'price': '5.50',
         'description': 'Wrap',
-        'code': 'W4'
+        'code': 'W4',
+        'imageUrl':"images/wraps.jpg"
     },
     'W5' : {
     	'productName': 'Orange Chicken',
         'productType' : 'Wraps',
         'price': '5.50',
         'description': 'Wrap',
-        'code': 'W5'
+        'code': 'W5',
+        'imageUrl':"images/wraps.jpg"
     },
     'W6' : {
     	'productName': 'Butter Chicken',
         'productType' : 'Wraps',
         'price': '5.50',
         'description': 'Wrap',
-        'code': 'W6'
+        'code': 'W6',
+        'imageUrl':"images/wraps.jpg"
 
     },
     'W7' : {
@@ -90,28 +102,32 @@ var menuCard = {
         'productType' : 'Wraps',
         'price': '5.50',
         'description': 'Wrap',
-        'code': 'W7'
+        'code': 'W7',
+        'imageUrl':"images/wraps.jpg"
     },
     'W8' : {
     	'productName': 'Philly Chicken',
     	'productType' : 'Wraps',
         'price': '5.50',
         'description': 'Wrap',
-        'code': 'W8'
+        'code': 'W8',
+        'imageUrl':"images/wraps.jpg"
     }, 
     'S1' : {
   		'productName': 'Classic Italian Veg-O-Rama Sub',
   		'productType' : 'Subs',
         'price': '5',
         'description': 'Lots of cheese, banana peppers, tomatoes and our house vinagrette toasted on a hoagie',
-        'code':'S1'
+        'code':'S1',
+        'imageUrl':"images/subs.jpg"
  	},
  	'S2' : {
   		'productName': 'Vegetarian \'Meat Ball\' Masterpiece',
   		'productType' : 'Subs',
             'price': '5',
             'description': '4 Crispy Golden \'Meat Balls\' slathered in BBQ sauce & Swiss Cheese toasted on a hoagie',
-            'code':'S2'
+            'code':'S2',
+            'imageUrl':"images/subs.jpg"
  	},
 
  	'BR1' : {
@@ -119,14 +135,16 @@ var menuCard = {
   		'productType' : 'Breakfast',
             'price': '5',
             'description': 'Breakfast',
-            'code': 'BR1'
+            'code': 'BR1',
+            'imageUrl':"images/breakfast2.jpg"
  	},
 	'BR2' : {
 		'productName': 'Amazng indian Tomato \'Omelette\'',
  		'productType' : 'Breakfast',
         'price': '5',
         'description': 'Breakfast',
-        'code': 'BR2'
+        'code': 'BR2',
+        'imageUrl':"images/breakfast2.jpg"
  	},
 
  	'BR3' : {
@@ -134,15 +152,57 @@ var menuCard = {
   		'productType' : 'Breakfast',
             'price': '5',
             'description': 'Breakfast',
-            'code': 'BR3'
+            'code': 'BR3',
+            'imageUrl':"images/breakfast2.jpg"
  	},
  	'BR4' : {
   		'productName': 'Chutney Sandwich',
   		'productType' : 'Breakfast',
             'price': '5',
             'description': 'Breakfast',
-            'code': 'BR4'
- 	}
+            'code': 'BR4',
+            'imageUrl':"images/breakfast2.jpg"
+     },
+     'P1':{
+        'productName': 'Popper',
+        'productType' : 'Optional',
+          'price': '3.49',
+          'description': 'Snack',
+          'code': 'P1',
+          'imageUrl':"images/breakfast2.jpg"
+     },
+     'SA1':{
+        'productName': 'Samosa',
+        'productType' : 'Optional',
+          'price': '2.99',
+          'description': 'Snack',
+          'code': 'SA1',
+          'imageUrl':"images/breakfast2.jpg"
+     },
+     'FR1':{
+        'productName': 'Fries',
+        'productType' : 'Optional',
+          'price': '2.99',
+          'description': 'Snack',
+          'code': 'FR1',
+          'imageUrl':"images/breakfast2.jpg"
+     },
+     'FRT1':{
+        'productName': 'Fritters',
+        'productType' : 'Optional',
+          'price': '3.99',
+          'description': 'Snack',
+          'code': 'FRT1',
+          'imageUrl':"images/breakfast2.jpg"
+     },
+     'BP':{
+        'productName': 'Butter Paneer',
+        'productType' : 'Customized',
+          'price': '5.49',
+          'description': 'This Snack is very tasty. I am writing thiis desc just to see how it looks. Since this modal is a little big',
+          'code': 'BP',
+          'imageUrl':"images/butterPaneer.jpg"
+     }
 
 	
 
@@ -154,7 +214,7 @@ var cartLine  = {
 	productType: '',
 	qty :0,
 	price : 0.0,
-	notes :''
+    notes :''
 }
 
 var cart = {
@@ -217,6 +277,12 @@ function displayWraps() {
     wrapArray.push(wrap8);
     return wrapArray;
 }
+function displayCustomizedItems(){
+ var customizedItemsArray =[];
+ var item1 = menuCard.BP;
+ customizedItemsArray.push(item1);
+ return customizedItemsArray
+}
 function displayBreakfastItems()
 {
     var bfArray = [];
@@ -230,15 +296,62 @@ function displayBreakfastItems()
     bfArray.push(bfItem4);
     return bfArray;
 }
-function addToCart(code){	    
-	if(menuCard[code]){
-		var cartLine = new Object();
-		cartLine.code = code;
-		cartLine.productName = menuCard[code].productName;
-		cartLine.productType = menuCard[code].productType;
-		cartLine.qty = 1;
-		cartLine.price = menuCard[code].price;
-		var cartObj ; 
+//After clicking Add to cart from pop up, fetch the menu item code, and other item code if any and call AddtoCart function
+function getItemCode()
+{
+    var code = document.getElementById('modalItemCode').innerText;  
+    var optionalItemCodes = getOptionalMenuItemCodes();    
+    this.addToCart(code);  
+    if (optionalItemCodes) {
+        optionalItemCodes.forEach(code => {           
+            this.addToCart(code);
+        })
+          
+        
+    }
+    this.hideModal();
+}
+//Depending on what items user has checked, create an array and push those codes in array
+function getOptionalMenuItemCodes()
+{
+    var optionalMenuItems = [];
+    var checkboxes = document.getElementsByName('typeOfAddOn');    
+  
+     for (let i = 0; i < checkboxes.length; i++)
+      {
+             if (checkboxes[i].checked)
+               {
+                   if(checkboxes[i].id=="chkPopper"){
+                      optionalMenuItems.push("P1");
+                   }
+                   else if(checkboxes[i].id =="chkSamosa"){
+                      optionalMenuItems.push("SA1")
+                   }
+                   else if(checkboxes[i].id =="chkFries"){
+                    optionalMenuItems.push("FR1")
+                 }
+                 else if(checkboxes[i].id =="chkFritters"){
+                    optionalMenuItems.push("FRT1")
+                 }
+               } 
+     
+     }
+     return optionalMenuItems;
+}
+
+function addToCart(code){	   
+    if(menuCard[code]){
+  
+   var cartLine = new Object();        
+    cartLine.code = code;
+    cartLine.productName = menuCard[code].productName;
+    cartLine.productType = menuCard[code].productType;
+    cartLine.qty = document.getElementById('modalItemQty').value;
+    cartLine.price = menuCard[code].price;
+    cartLine.notes = document.getElementById('txtNotes').value;
+    var cartObj ;         
+     
+   
 		if(typeof(Storage) !== "undefined") {
 			if (sessionStorage.getItem( "vegOramaCart" )) {
 				// cart already exists in the memory
@@ -247,7 +360,8 @@ function addToCart(code){
 				var found = false;
 				for (i = 0; i < cartObj.cartLine.length && found == false; i++) {
 					if(cartObj.cartLine[i].code == code){
-						cartObj.cartLine[i].qty = cartObj.cartLine[i].qty + 1; 
+                        cartObj.cartLine[i].qty = document.getElementById('modalItemQty').value
+                        cartObj.cartLine[i].notes = document.getElementById('txtNotes').value
 						found = true; 
 					}
 				}
@@ -261,12 +375,15 @@ function addToCart(code){
 
 				// create new object 
 				cartObj = new Object();
-				cartObj.cartLine = [cartLine];
+                cartObj.cartLine = [cartLine];
+                
+                
             }
 
 			cartObj = calculateTotalPrice(cartObj);
 			var jsonStr = JSON.stringify( cartObj );
             sessionStorage.setItem("vegOramaCart", jsonStr);
+          
           
            
 		} else {
@@ -288,11 +405,10 @@ function calculateTotalPrice(cartObj) {
         totalPrice = cartObj.cartLine[i].price * cartObj.cartLine[i].qty + Number(totalPrice);
         totalQuantity = totalQuantity + cartObj.cartLine[i].qty;
 	}
-	cartObj.totalPrice = Number(totalPrice); 
+	cartObj.totalPrice = Number(totalPrice).toFixed(2); 
 	cartObj.salesTax = (totalPrice * 0.06).toFixed(2) ; 
-    cartObj.finalTotal = Number(cartObj.totalPrice) + Number(cartObj.salesTax); 
-    cartObj.totalQuantity = Number(totalQuantity);
-    document.getElementById('dvQuantity').innerHTML = "(" + cartObj.totalQuantity + ")"; //Everytime user clicks add to cart, dvQuantity is updated with the quantity
+    cartObj.finalTotal = (Number(cartObj.totalPrice) + Number(cartObj.salesTax)).toFixed(2); 
+    cartObj.totalQuantity = Number(totalQuantity).toFixed(2); 
 	return cartObj; 
 
 }
@@ -361,17 +477,62 @@ function deleteCartItem(rowId, code) {
     document.getElementById('cartTable').remove('rowId');
 }
 
+//Generate different HTML depending on the type of item
 function openPopUp(code) {
-	var modal = document.getElementById('myModal');
-	modal.style.display = "block";
-	if(menuCard[code]){
-		document.getElementById('modalItemName').innerHTML  = menuCard[code].productName;
-		document.getElementById('modalItemDesc').innerHTML  = menuCard[code].description;
-		document.getElementById('modalItemPrice').innerHTML = '$' + menuCard[code].price;
-		document.getElementById('modalItemCode').innerHTML = code;
-	}
+   
+    if(code == 'B1' || code=='B2'|| code == 'B3' || code=='B4' || code=='B5'||code=='B6')
+    {   
+
+       generateHtmlForBurgerPopUp(code);
+        
+    }
+    else if(code =='BP')
+    {   
+       generateHtmlCustomizedItems(code);
+    }
+    else
+    {
+        generatePopUpHtmlForGenericItems(code);
+       
+    }
+  
+    
 }
 
+function generateHtmlCustomizedItems(code)
+{
+    generatePopUpHtmlForGenericItems(code);    
+    var modal = document.getElementById('trCustomStyle');
+    modal.style.display = "block";
+   
+    var modal1 = document.getElementById('trCustomExtra');
+    modal1.style.display = "block";
+}
+function generateHtmlForBurgerPopUp()
+{
+    generatePopUpHtmlForGenericItems(code); 
+}
+function generatePopUpHtmlForGenericItems(code)
+{
+    var modal = document.getElementById('myModal');
+    modal.style.display = "block";
+  
+    if(menuCard[code]){
+        document.getElementById('modalItemName').innerHTML  = menuCard[code].productName;
+        document.getElementById('modalItemDesc').innerHTML  = menuCard[code].description;
+        document.getElementById('modalItemPrice').innerHTML = '$' + menuCard[code].price;
+        document.getElementById('modalItemCode').innerHTML = code;
+        document.getElementById('imgMenuItem').src = menuCard[code].imageUrl;
+        
+    }
+    setPriceAddToBagBtn();
+ 
+}
+function setPriceAddToBagBtn()
+{
+    document.getElementById('btnAddToBag').value ="Add to bag:" + document.getElementById('modalItemPrice').innerHTML;
+    
+}
 function hideModal(){
 	var modal = document.getElementById('myModal');
 	 modal.style.display = "none";
@@ -393,19 +554,93 @@ function decrementValue()
 	}
     document.getElementById('modalItemQty').value = value;
 }
+function calculateBowlPrice() {
+    var currentPrice;
+    var rdTypeOfStyle = document.getElementsByName('typeOfItem'); 
+    for (let i = 0; i < rdTypeOfStyle.length; i++) {
 
-function addExtraCharges(element){
-	if(element.checked) {
-		var currentPrice = document.getElementById('modalItemPrice').innerHTML ;
-		var res = currentPrice.replace("$", "");
-		currentPrice = Number(res);
-		currentPrice = (currentPrice + 1).toFixed(2) ;
-		document.getElementById('modalItemPrice').innerHTML  = '$'+currentPrice;
-	} else {
-		var currentPrice = document.getElementById('modalItemPrice').innerHTML ;
-		var res = currentPrice.replace("$", "");
-		currentPrice = Number(res);
-		currentPrice =(currentPrice - 1).toFixed(2) ;
-		document.getElementById('modalItemPrice').innerHTML  = '$'+currentPrice;
-	}
+        if (rdTypeOfStyle[i].checked && rdTypeOfStyle[i].id == "rdBowl") {
+            currentPrice = getCurrentItemPrice();
+            currentPrice = (currentPrice + parseFloat(rdTypeOfStyle[i].value)).toFixed(2);
+            setCurrentPrice(currentPrice)
+        }
+
+        else if (rdTypeOfStyle[i].id == "rdWrap" || rdTypeOfStyle[i].id == "rdPizza") {
+
+            currentPrice = parseFloat(rdTypeOfStyle[i].value).toFixed(2);
+            setCurrentPrice(currentPrice)
+
+        }
+        else {
+            currentPrice = getCurrentItemPrice();
+        }
+    }
+    setPriceAddToBagBtn();
+}
+function calculateBowlRice() {
+    var currentPrice;
+    var checkboxesRice = document.getElementsByName('typeOfRice'); 
+    for (let i = 0; i < checkboxesRice.length; i++) {
+        //checkboxesRice[i].addEventListener('change', () => { /I had added this,coz when user unchecks we wanted this to be called again
+
+            if (checkboxesRice[i].checked) {
+                currentPrice = getCurrentItemPrice();
+                currentPrice = (currentPrice + 1).toFixed(2);
+                setCurrentPrice(currentPrice);
+             
+
+            } else {
+                if (currentPrice >= 0) {
+                    currentPrice = getCurrentItemPrice();
+                    currentPrice = (currentPrice - 1).toFixed(2);
+                    setCurrentPrice(currentPrice)
+                }
+            }
+        //});
+    }
+    setPriceAddToBagBtn();
+}
+
+    //Namita Please check this addEvent Listener thing,it doesnt fire when first checbox is clicked because it does'nt trigger onclick there.
+function calculateOptionalItemsPrice() {
+    var currentPrice;
+    var checkboxesAddOn = document.getElementsByName('typeOfAddOn');
+    for (let i = 0; i < checkboxesAddOn.length; i++) {
+        checkboxesAddOn[i].addEventListener('click', () => {
+            if (checkboxesAddOn[i].checked) {
+                currentPrice = getCurrentItemPrice();
+                currentPrice = (currentPrice + parseFloat(checkboxesAddOn[i].value)).toFixed(2);               
+                setCurrentPrice(currentPrice);
+            
+
+            } else {
+                if (currentPrice >= 0) {
+                    currentPrice = getCurrentItemPrice();
+                    currentPrice = (currentPrice - parseFloat(checkboxesAddOn[i].value)).toFixed(2);
+                    setCurrentPrice(currentPrice)
+                }
+            }
+            
+        });
+    }
+    setPriceAddToBagBtn();
+}
+function setCurrentPrice(currentPrice) {
+    document.getElementById('modalItemPrice').innerHTML = '$' + currentPrice
+ 
+}
+function getCurrentItemPrice() {
+    var currentPrice;
+    var res; 
+    currentPrice = document.getElementById('modalItemPrice').innerHTML;
+    res = currentPrice.replace("$", "");
+    currentPrice = Number(res);
+    return currentPrice;
+
+}
+function addExtraCharges(element)
+{
+    //Add price for optional menu items
+    calculateOptionalItemsPrice();   
+    setPriceAddToBagBtn();
 }
